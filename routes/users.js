79121e5daa00fad09e67ff9b34ac1015ee3e0785
simit namdev <simitnamdev027@gmail.com>
2024-10-29 09:59:17 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+/*var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;*/
+
+
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards:{
+    type: Array,
+    default: []
+  },
+  posts: [
+    {
+      type: mongoose. Schema.Types.ObjectId,
+      ref: "post"
+    }
+  ]
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema)
